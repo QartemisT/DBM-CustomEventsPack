b3012f5c6b211239c08f4c05424b723a1db94d17
Artemis <QartemisT@gmail.com>
2021-08-30 04:23:37 +0100

    Update to new API usage

--- LoadList.lua
+++ LoadList.lua
@@ -1,66 +1,41 @@
 local insert = table.insert
 local inserted1, inserted2, inserted3 = false, false, false
 
-function DBMVPCustomEventsPack() -- Register Victory sounds to DBM.Victory table
+function DBMVPCustomEventsPack() -- Register Victory sounds
 	if inserted1 then
 		return
 	end
 	-- Make sure EACH entry has a UNIQUE text
-	--[[
-	insert(DBM.Victory, {
-		text	= "Your custom victory sound",
-		value	= "Interface\\AddOns\\DBM-CustomEventsPack\\Victory\\CustomSound1.ogg"
-	})
-	--]]
+	-- DBM:RegisterVictorySound("Your custom victory sound", "Interface\\AddOns\\DBM-CustomEventsPack\\Victory\\CustomSound1.ogg")
 	inserted1 = true
 end
 
-function DBMDPCustomEventsPack() -- Register Defeat sounds to DBM.Defeat Table
+function DBMDPCustomEventsPack() -- Register Defeat sounds
 	if inserted2 then
 		return
 	end
 	-- Make sure EACH entry has a UNIQUE text
-	--[[
-	insert(DBM.Defeat, {
-		text	= "Your custom defeat sound",
-		value	= "Interface\\AddOns\\DBM-CustomEventsPack\\Defeat\\CustomSound1.ogg"
-	})
-	--]]
+	-- DBM:RegisterDefeatSound("Your custom defeat sound", "Interface\\AddOns\\DBM-CustomEventsPack\\Defeat\\CustomSound1.ogg")
 	inserted2 = true
 end
 
-function DBMMPCustomEventsPack() -- Register Music to DBM.Music Table
+function DBMMPCustomEventsPack() -- Register Music
 	if inserted3 then
 		return
 	end
 	-- Make sure EACH entry has a UNIQUE text
-	--[[
-	insert(DBM.Music, {
-		text	= "Your custom music sound",
-		value	= "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound1.ogg"
-	})
-	--]]
+	-- DBM:RegisterMusic("Your custom music sound", "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound1.ogg")
 
 	-- Dungeon BGM Table
 	if DBM.DungeonMusic then
 		-- Make sure EACH entry has a UNIQUE text
-		--[[
-		insert(DBM.DungeonMusic, {
-			text	= "Your custom dungeon music sound",
-			value	= "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound2.ogg"
-		})
-		--]]
+		-- DBM:RegisterDungeonMusic("Your custom dungeon music sound", "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound2.ogg")
 	end
 
 	-- Boss BGM Table
 	if DBM.BattleMusic then
 		-- Make sure EACH entry has a UNIQUE text
-		--[[
-		insert(DBM.BattleMusic, {
-			text	= "Your custom dungeon music sound",
-			value	= "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound3.ogg"
-		})
-		--]]
+		-- DBM:RegisterBattleMusic("Your custom battle music sound", "Interface\\AddOns\\DBM-CustomEventsPack\\Music\\CustomSound3.ogg")
 	end
 	inserted3 = true
 end
